e72666e78487a263505cf757fc59faeef9e6d994
Sayasat Sabit <sayasatsabit@MacBook-Air-Sayasat.local>
2025-01-19 16:49:02 +0500

    swagger updated

--- src/main/java/com/programming/firebaselearnauth/security/SecurityConfig.java
+++ src/main/java/com/programming/firebaselearnauth/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http.
                 csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/verify-token").permitAll()
+                        .requestMatchers( "/v3/api-docs","swagger-ui.html","/verify-token").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new FirebaseAuthFilter(), UsernamePasswordAuthenticationFilter.class);
